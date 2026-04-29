fd266b959f6cc0e48ca1670a8c8110f4efa1e217
zkambo <zoe.a.kambouris@student.uts.edu.au>
2026-04-29 16:26:58 +1000

    Update traits_query_backend.py

--- App/templates/traits_query_backend.py
+++ App/templates/traits_query_backend.py
@@ -1,20 +1,15 @@
-" Seperate traits backend from query_builder right now so I don't mess with any of Johns work :)"
+# WIP: Trait filtering logic
+# Separated from query_builder to avoid interfering with ongoing UI design
+# This will be implemented once species_traits_junctions table is finalised
 
-def apply_trait_filters(query, request, Species, Trait, SpeciesTrait):
-
+def apply_trait_filters(query, request):
+    
+    # Placeholder for future trait filters
     trait_name = request.form.get('trait_name')
     trait_value = request.form.get('trait_value')
 
+    # Currently not applied as trait-related tables and relationships are still under development
     if trait_name or trait_value:
-        query = query.join(SpeciesTrait, Species.species_id == SpeciesTrait.species_id)\
-                     .join(Trait, Trait.trait_id == SpeciesTrait.trait_id)
-
-        if trait_name:
-            query = query.filter(Trait.trait_name == trait_name)
-
-        if trait_value:
-            query = query.filter(SpeciesTrait.value.ilike(f"%{trait_value}%"))
-
-        query = query.distinct()
+        pass  # To be implemented once database schema is complete
 
     return query
